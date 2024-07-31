a401b6316d9878b053848b1f316922a6b8af682c
dojnny <95517085+dojnny@users.noreply.github.com>
2024-07-31 09:26:47 -0700

    Update spi_slave.v

--- spi_slave.v
+++ spi_slave.v
@@ -1,29 +1,4 @@
-// ---------------------------------------------------------------------------
-// © 2022 Renesas Electronics
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
-// OR OTHER DEALINGS IN THE SOFTWARE.
-// ---------------------------------------------------------------------------
-// Base Module Name: spi_slave
-// Target Devices: SLG47910
-// Tools version:
-//   Software: ForgeFPGA Workshop v.6.33
-//   Hardware: FPGAPAK Development Board Rev.1.1
-// Revision:
-//   06.09.2021 r001 - New design
-//   09.19.2022 r002 - Code style review
-//   03.25.2024 r003 - Changed output enable polarity
-//   04.02.2024 r004 - Limits added in the parameters comments
-// ---------------------------------------------------------------------------
-// Description :
-//   spi slave (Serial Peripheral Interface) is a 4-wire synchronous serial communication interface.
+
 
 `timescale 1ns / 1ps
 
