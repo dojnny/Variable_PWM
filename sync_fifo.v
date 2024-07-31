732a505790dd03672b30a511151c68bca53d6aea
dojnny <95517085+dojnny@users.noreply.github.com>
2024-07-30 20:27:06 -0700

    Update sync_fifo.v

--- sync_fifo.v
+++ sync_fifo.v
@@ -1,29 +1,3 @@
-// ---------------------------------------------------------------------------
-// © 2023 Renesas Electronics
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
-// OR OTHER DEALINGS IN THE SOFTWARE.
-// ---------------------------------------------------------------------------
-// Base Module Name: sync_fifo
-// Target Device: SLG47910
-// Tools version:
-//   Software: ForgeFPGA Workshop v.6.34
-//   Hardware: FPGAPAK Development Board Rev.1.1
-// Revision:
-//   07.08.2021 r001 - New design
-//   03.15.2023 r002 - Code style review
-// ---------------------------------------------------------------------------
-// Description :
-//   The synchronous FIFO  module - is one of the types of memory (first in, first out), which used for in-order data storage and retrieval.
-// ---------------------------------------------------------------------------
-
 `timescale 1ns/1ps
 
 module sync_fifo #(
